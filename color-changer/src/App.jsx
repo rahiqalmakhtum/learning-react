4bdec1138603370e86c05bd8d0e8a3feb5096257
rahiqalmakhtum <rahiq789@gmail.com>
2025-12-29 17:12:11 +0600

    added project "Color changer"

---
+++ color-changer/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("");
+
+  return (
+    <div className="h-screen w-screen" style={{ backgroundColor: color }}>
+      {/* without flex-wap the width of the container becomes full width of the
+      screen */}
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => {
+              setColor("red");
+              console.log(color);
+            }}
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-lg"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white font-semibold shadow-lg"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
