628ba63e89144ddd6fa001de4126270586c06889
rahiqalmakhtum <rahiq789@gmail.com>
2026-05-06 16:14:27 +0600

    practiced useEffect hook

--- hooks-practice/src/App.jsx
+++ hooks-practice/src/App.jsx
@@ -30,3 +30,56 @@
 
 // export default App;
 // =================useState Hook========================
+
+//==================useEffect Hook=======================
+// import React from "react";
+// import { useState, useEffect } from "react";
+
+// function App() {
+//   const [resourceType, setResourceType] = useState("posts");
+//   const [items, setItems] = useState([]);
+
+//   console.log("render");
+
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+//       .then((response) => response.json())
+//       .then((json) => setItems(json));
+//   }, [resourceType]);
+//   return (
+//     <>
+//       <button onClick={() => setResourceType("Posts")}>Posts</button>
+//       <button onClick={() => setResourceType("Users")}>Users</button>
+//       <button onClick={() => setResourceType("Comments")}>Comments</button>
+//       <h1>{resourceType}</h1>
+//       {items.map((items) => {
+//         return <pre>{JSON.stringify(items)}</pre>;
+//       })}
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    // This runs when the component is unmounted/destroyed, and it removes the event listener to prevent memory leaks.
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <>{windowWidth}</>;
+}
+
+export default App;
