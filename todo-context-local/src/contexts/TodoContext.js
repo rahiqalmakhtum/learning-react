83a4658a37706cf9a9a532d323bd6f6b0832c951
rahiqalmakhtum <rahiq789@gmail.com>
2026-05-12 16:25:55 +0600

    completed project todo-context-local

--- todo-context-local/src/contexts/TodoContext.js
+++ todo-context-local/src/contexts/TodoContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const TodoContext = createContext({
     todos: [{
         id: 1,
-        title: "Learn React",
+        todo: "Learn React",
         completed: false
     }],
     addTodo: (todo) => {},
